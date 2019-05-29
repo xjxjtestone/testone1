6dea01a3913f2a86ae6d4e6389890367417cec54
xiejun <2826499004@qq.com>
2019-05-29 14:47:01 +0800

    alert

--- test.java
+++ test.java
@@ -2,5 +2,5 @@ class HelloWorld
 {    
  /**     * 这是一个 main 方法     */   
  public static void main(String [] args) 
-{        // 输出此消息      System.out.println("欢迎来到 Java 世界");    }
+{        // 输出此消息      System.out.println("欢迎来到 小俊哥的 世界");    }
 }
